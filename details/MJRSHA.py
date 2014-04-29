3ade7dbb0922c8a69576be6ff5e671e561df7254
Michael Reschly <michael@reschly.com>
2014-04-28 21:08:56 -0400

    add SHA constants, padding creation

--- details/MJRSHA.py
+++ details/MJRSHA.py
@@ -9,51 +9,103 @@ class SHA(object):
     '''
     
     @staticmethod
-    def ch(x, y, z, bitlen=32):
+    def _ch(x, y, z, bitlen=32):
         '''FIPS 180-4, Section 4.1.2/4.1.3'''
         return (x & y) ^ (NOT(x, bitlen) & z)
     
     @staticmethod
-    def maj(x, y, z):
+    def _maj(x, y, z):
         '''FIPS 180-4, Section 4.1.2/4.1.3'''
         return (x & y) ^ (x & z) ^ (y & z)
     
     @staticmethod
-    def bigsigma0_256(x):
+    def _bigsigma0_256(x):
         '''FIPS 180-4, Section 4.1.2'''
         return ROTR(x, 2, bitlen=32) ^ ROTR(x, 13, bitlen=32) ^ ROTR(x, 22, bitlen=32)
     
     @staticmethod
-    def bigsigma1_256(x):
+    def _bigsigma1_256(x):
         '''FIPS 180-4, Section 4.1.2'''
         return ROTR(x, 6, bitlen=32) ^ ROTR(x, 11, bitlen=32) ^ ROTR(x, 25, bitlen=32)
     
     @staticmethod
-    def littlesigma0_256(x):
+    def _littlesigma0_256(x):
         '''FIPS 180-4, Section 4.1.2'''
         return ROTR(x, 7, bitlen=32) ^ ROTR(x, 18, bitlen=32) ^ SHR(x, 3, bitlen=32)
     
     @staticmethod
-    def littlesigma1_256(x):
+    def _littlesigma1_256(x):
         '''FIPS 180-4, Section 4.1.2'''
         return ROTR(x, 17, bitlen=32) ^ ROTR(x, 19, bitlen=32) ^ SHR(x, 10, bitlen=32)
     
     @staticmethod
-    def bigsigma0_512(x):
+    def _bigsigma0_512(x):
         '''FIPS 180-4, Section 4.1.2'''
         return ROTR(x, 28, bitlen=64) ^ ROTR(x, 34, bitlen=64) ^ ROTR(x, 39, bitlen=64)
     
     @staticmethod
-    def bigsigma1_512(x):
+    def _bigsigma1_512(x):
         '''FIPS 180-4, Section 4.1.2'''
         return ROTR(x, 14, bitlen=64) ^ ROTR(x, 18, bitlen=64) ^ ROTR(x, 41, bitlen=64)
     
     @staticmethod
-    def littlesigma0_512(x):
+    def _littlesigma0_512(x):
         '''FIPS 180-4, Section 4.1.2'''
         return ROTR(x, 1, bitlen=64) ^ ROTR(x, 8, bitlen=64) ^ SHR(x, 7)
     
     @staticmethod
-    def littlesigma1_512(x):
+    def _littlesigma1_512(x):
         '''FIPS 180-4, Section 4.1.2'''
         return ROTR(x, 19, bitlen=64) ^ ROTR(x, 61, bitlen=64) ^ SHR(x, 6)    
+    
+    # SHA-256 constants, FIPS 180-4 Section 4.2.2
+    __K256 = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+              0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+              0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+              0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+              0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+              0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+              0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+              0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2]
+    
+    # SHA-384 / SHA-512 constants, FIPS 180-4 Section 4.2.3
+    __K512 = [0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc,
+              0x3956c25bf348b538, 0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118,
+              0xd807aa98a3030242, 0x12835b0145706fbe, 0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2,
+              0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235, 0xc19bf174cf692694,
+              0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65,
+              0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5,
+              0x983e5152ee66dfab, 0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4,
+              0xc6e00bf33da88fc2, 0xd5a79147930aa725, 0x06ca6351e003826f, 0x142929670a0e6e70,
+              0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 0x53380d139d95b3df,
+              0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b,
+              0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30,
+              0xd192e819d6ef5218, 0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8,
+              0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8,
+              0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373, 0x682e6ff3d6b2b8a3,
+              0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec,
+              0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b,
+              0xca273eceea26619c, 0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178,
+              0x06f067aa72176fba, 0x0a637dc5a2c898a6, 0x113f9804bef90dae, 0x1b710b35131c471b,
+              0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc, 0x431d67c49c100d4c,
+              0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817]
+    
+    @staticmethod
+    def _create_padding(length, block_size):
+        # Creates a padding string for a SHA message.  See FIPS 180-4 Section 5.1
+        # Only supports whole-byte messages (bitlength = 0 mod 8)
+        # length is in bits
+        
+        # size of the last block prior to length value
+        desired_size = (block_size * 7) // 8   # Note that 448 = (7/8) * 512, 896 = (7/8) * 1024
+        # size of the length value
+        length_size = (block_size // 8)
+        # amount of zero bytes = (desired size - (data)) mod block_size
+        zero_bytes = desired_size - ((length + 1)%  block_size)
+        if (zero_bytes < 0):
+            zero_bytes += block_size
+        
+        # padding = 0x80 + zeroes + length
+        padding = b'\x80' + b'\x00' * (zero_bytes) + length.to_bytes(block_size//64, order='big')
+        
+        return padding
