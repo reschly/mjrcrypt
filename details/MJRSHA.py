071342235cfcb44de32ca93273f2333962356fb2
Michael Reschly <michael@reschly.com>
2014-04-29 01:27:10 -0400

    add initial H values for SHA

--- details/MJRSHA.py
+++ details/MJRSHA.py
@@ -109,3 +109,33 @@ class SHA(object):
         padding = b'\x80' + b'\x00' * (zero_bytes) + length.to_bytes(block_size//64, order='big')
         
         return padding
+    
+    # Initial H values for SHA-256, FIPS 180-4 Section 5.3.3
+    __initial_h_256 = [0x6a09e667,
+                       0xbb67ae85,
+                       0x3c6ef372,
+                       0xa54ff53a,
+                       0x510e527f,
+                       0x9b05688c,
+                       0x1f83d9ab,
+                       0x5be0cd19]
+    
+    # Initial H values for SHA-384, FIPS 180-4 Section 5.3.4    
+    __initial_h_384 = [0xcbbb9d5dc1059ed8,
+                       0x629a292a367cd507,
+                       0x9159015a3070dd17,
+                       0x152fecd8f70e5939,
+                       0x67332667ffc00b31,
+                       0x8eb44a8768581511,
+                       0xdb0c2e0d64f98fa7,
+                       0x47b5481dbefa4fa4]   
+
+    # Initial H values for SHA-512, FIPS 180-4 Section 5.3.5    
+    __initial_h_512 = [0x6a09e667f3bcc908,
+                       0xbb67ae8584caa73b,
+                       0x3c6ef372fe94f82b,
+                       0xa54ff53a5f1d36f1,
+                       0x510e527fade682d1,
+                       0x9b05688c2b3e6c1f,
+                       0x1f83d9abfb41bd6b,
+                       0x5be0cd19137e2179]
